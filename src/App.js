0f1b8826244984a188d95456254f973b9d2bfc9a
Mystist <gjl87910lq@gmail.com>
2017-08-23 13:59:31 +0800

    feat: scaffold for static components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+import * as Static from './Static'
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div className="app">
+        <Route path="/static/post-list" component={Static.PostList} ></Route>
+        <Route path="/static/post-detail" component={Static.PostDetail} ></Route>
+      </div>
     )
   }
 }
---
+++ src/Static.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function PostList() {
+  return (
+    <h1>Post List</h1>
+  )
+}
+
+function PostDetail() {
+  return (
+    <h1>Post Detail</h1>
+  )
+}
+
+export { PostList, PostDetail }
